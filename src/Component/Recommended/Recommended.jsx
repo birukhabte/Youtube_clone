6416ca398d21ffc8e0d78b2e349a1bf14be841de
Biruk Habte <biruk.habte-ug@aau.edu.et>
2026-02-19 09:59:33 +0300

    implement Recommended component to fetch and display popular videos based on category ID.

--- src/Component/Recommended/Recommended.jsx
+++ src/Component/Recommended/Recommended.jsx
@@ -9,16 +9,16 @@ import thumbnail6 from '../../assets/thumbnail6.png'
 import thumbnail7 from '../../assets/thumbnail7.png'
 import thumbnail8 from '../../assets/thumbnail8.png'
 
-import { API_KEY } from '../../data'
-import { value_converter } from '../../data'
+import { API_KEY } from '../../Data'
+import { value_converter } from '../../Data'
 import { Link } from 'react-router-dom'
 
 
 
-function Recommended({categoryId}) {
+function Recommended({ categoryId }) {
     const [apiData, setApiData] = useState([])
     const fetchData = async () => {
-    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
+        const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`
         await fetch(videoList_url)
             .then(res => res.json())
             .then(data => setApiData(data.items || []));
